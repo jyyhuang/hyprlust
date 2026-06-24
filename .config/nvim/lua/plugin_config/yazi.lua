529fec1de04c7cfb7e537f261688ceab0552df12
Joey Huang <joeyhuang.dev@gmail.com>
2026-06-24 03:00:22 -0400

    refactor: moved miku jpg to wallpapers

--- .config/nvim/lua/plugin_config/yazi.lua
+++ .config/nvim/lua/plugin_config/yazi.lua
@@ -1,15 +1,17 @@
 return {
 	"mikavilpas/yazi.nvim",
-	cmd = "Yazi",
+	version = "*",
+	event = "VeryLazy",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", lazy = true },
+	},
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = true,
+		open_for_directories = false,
 		floating_window_scaling_factor = 1,
 		yazi_floating_window_border = "none",
 	},
 	config = function()
-		vim.keymap.set("n", "-", function()
-			require("yazi").yazi()
-		end, { desc = "Open yazi" })
+		vim.keymap.set("n", "-", "<cmd>Yazi<cr>", { desc = "Open yazi" })
 	end,
 }
